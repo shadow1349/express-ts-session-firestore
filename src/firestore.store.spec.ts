b01dd065156c3f41e493a0ff076595e80ddb0bf4
Sam Redmond <sredmond@techusolutions.com>
2023-12-17 08:53:33 -0600

    Adding initial test file and husky pre-commit test

--- src/firestore.store.spec.ts
+++ src/firestore.store.spec.ts
@@ -1,12 +1,26 @@
 import { Firestore } from "@google-cloud/firestore";
 import { FirestoreStore } from "./firestore.store";
 
+const mockFirestore = {
+  collection: jest.fn().mockReturnThis(),
+  doc: jest.fn().mockReturnThis(),
+  set: jest.fn(),
+};
+
 describe("firestore.store", () => {
   let db: Firestore;
   let store: FirestoreStore;
 
   beforeEach(() => {
     db = new Firestore();
+
+    db.collection = mockFirestore.collection;
+    db.doc = mockFirestore.doc;
+
     store = new FirestoreStore({ database: db });
+  });
+
+  it("should be defined", () => {
+    expect(store).toBeDefined();
   });
 });
